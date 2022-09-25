2535e337d188ba3b13af0858cdb2af944202e025
Hiba Hasan <hibaha638@gmail.com>
2022-09-25 13:29:32 +0000

    Finish crispy forms

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,13 @@ from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey,GenericRelation
 from django.contrib.contenttypes.models import ContentType
 
+
 class Tag(models.Model):
   value = models.TextField(max_length=100)
 
   def __str__(self):
     return self.value
+
 
 class Comment(models.Model):
   creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -17,6 +19,7 @@ class Comment(models.Model):
   content_object = GenericForeignKey("content_type", "object_id")
   created_at = models.DateTimeField(auto_now_add=True)
   modified_at = models.DateTimeField(auto_now=True)
+
 
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
