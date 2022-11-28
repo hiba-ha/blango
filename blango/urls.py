c6a722ba0dd68eeee9da0f0d12fc33ff93157dde
Hiba Hasan <hibaha638@gmail.com>
2022-11-28 13:36:02 +0000

    Finish django registration

--- blango/urls.py
+++ blango/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import blog.views
+import blango_auth.views
 import debug_toolbar
+from blango_auth.forms import BlangoRegistrationForm
+from django_registration.backends.activation.views import RegistrationView
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
@@ -25,6 +28,15 @@ urlpatterns = [
     path('', blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", blango_auth.views.profile, name="profile"),
+    path(
+    "accounts/register/",
+    RegistrationView.as_view(form_class=BlangoRegistrationForm),
+    name="django_registration_register",
+    ),
+    path("accounts/", include("django_registration.backends.activation.urls")),
+
 ]
 
 if settings.DEBUG:
