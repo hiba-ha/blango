840fbfe3ddb3b106c5aeff24b44c133d56faae27
Hiba Hasan <hibaha638@gmail.com>
2022-12-01 11:42:07 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -51,12 +51,18 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'debug_toolbar',
         'crispy_forms',
         'crispy_bootstrap5',
         'blango_auth',
-        'blog'
+        'blog',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google'
+
     ]
 
     INTERNAL_IPS = ["192.168.10.226"]
@@ -201,6 +207,13 @@ class Dev(Configuration):
     AUTH_USER_MODEL = "blango_auth.User"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
+
+    SITE_ID = 1
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 
 class Prod(Dev):
